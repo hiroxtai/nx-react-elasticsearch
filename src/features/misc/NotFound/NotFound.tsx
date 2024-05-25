b04a7df88be1c5f757b4427e14aba23310e27458
hiroxtai <44028477+hiroxtai@users.noreply.github.com>
2024-05-25 10:58:55 +0000

    NotFound コンポーネントを追加

---
+++ src/features/misc/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface NotFoundProps {}
+
+export function NotFound(props: NotFoundProps) {
+  return (
+    <div>
+      <h1>Welcome to NotFound!</h1>
+    </div>
+  );
+}
+
+export default NotFound;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
 import { Home } from '@/features/Home';
+import { NotFound } from '@/features/misc';
 import { useRoutes } from 'react-router-dom';
 
 export const AppRoutes = () => {
-  const routes = [{ path: '/', element: <Home /> }];
+  const routes = [
+    { path: '/', element: <Home /> },
+    { path: '*', element: <NotFound /> },
+  ];
 
   const element = useRoutes([...routes]);
 
