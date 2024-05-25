a114f6ae6e4a961b8271fc2ca403fb1bf373ab20
hiroxtai <44028477+hiroxtai@users.noreply.github.com>
2024-05-25 16:44:42 +0000

    Elasticにブラウザからアクセスしてデータが取得できることを確認

--- src/features/Elasticsearch/Elasticsearch.tsx
+++ src/features/Elasticsearch/Elasticsearch.tsx
@@ -1,7 +1,27 @@
+import elasticsearch from 'elasticsearch-browser';
+import { useEffect } from 'react';
+
 /* eslint-disable-next-line */
 export interface ElasticsearchProps {}
 
 export function Elasticsearch(props: ElasticsearchProps) {
+  useEffect(() => {
+    const client = new elasticsearch.Client({ host: 'http://localhost:9200' });
+
+    (async () => {
+      const result = await client.search({
+        index: 'kibana_sample_data_logs',
+        body: {
+          query: {
+            match_all: {},
+          },
+        },
+      });
+
+      console.log(result);
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Welcome to Elasticsearch!</h1>
